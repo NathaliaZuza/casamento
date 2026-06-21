647c159a996e078f2629352d802e6f0f53e9c87e
Nathália Zuza <nathalia555zuza@gmail.com>
2026-06-21 15:38:56 -0300

    adicionando img de compartilhar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,31 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL ??
-  (process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000");
+const siteUrl = "https://casamentonathaliaerodrigo.com.br/";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
   title: "Rodrigo & Nathália",
   description: "Casamento • 06 de Agosto de 2026",
+
   openGraph: {
     title: "Rodrigo & Nathália",
     description: "Casamento • 06 de Agosto de 2026",
     type: "website",
+
+    images: [
+      {
+        url: `${siteUrl}/og-image.jpg`,
+        width: 1200,
+        height: 630,
+        alt: "Rodrigo & Nathália",
+      },
+    ],
   },
+
   twitter: {
     card: "summary_large_image",
+    images: [`${siteUrl}/og-image.jpg`],
   },
 };
 export default function RootLayout({
