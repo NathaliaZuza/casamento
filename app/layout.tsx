fc6fc66c43ec6fa1af06f8f61c89eab1e45a81a0
Nathália Zuza <nathalia555zuza@gmail.com>
2026-06-21 15:42:29 -0300

    aaaaa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-const siteUrl = "https://casamentonathaliaerodrigo.com.br/";
+const siteUrl = "https://casamentonathaliaerodrigo.com.br";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
